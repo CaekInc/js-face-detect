0489f1a2e18aad569df86aa1a9b932962b3103a0
CaekInc <caekinc@gmail.com>
2023-01-30 12:07:08 +0200

    #WIP: init repo

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 /* @refresh reload */
-import 'windi.css';
+import './index.css';
 import { render } from 'solid-js/web';
 
 import App from './App';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+  ],
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mob': '430px',
+      },
+      colors: {
+        transparent: 'transparent',
+        'black': '#000000',
+        'blue': {
+          DEFAULT: '#3579f6',
+          light: '#0F6BFF'
+        },
+        'gray': {
+          light: '#797979',
+          DEFAULT: '#808080',
+          dark: '#999999'
+        }
+      }
+    },
   },
   plugins: [],
 };
